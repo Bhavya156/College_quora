15ce655b954fc5c9c0b2f0cd1a97ecd213df6314
Shreyans Joshi <80451337+joshishreyans@users.noreply.github.com>
2022-02-07 23:53:39 +0530

    added security using hasing of passowrds

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,9 +1,11 @@
 //-FILE DESCRIPTION: Used to define user schema
 
 const mongoose = require("mongoose"); //acquired mongoose
+const bcrypt = require("bcryptjs"); //acquired bcrypt
 
-//schema definition
-const userSchema = new mongoose.Schema({ 
+//****************schema definition**********************//
+
+const userSchema = new mongoose.Schema({
   fName: {
     type: String,
     required: true,
@@ -20,6 +22,16 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+
+//****************start of method to hash passwords****************//
+
+userSchema.pre("save", async function (next) {  //method is ran before save method hence pre-save method
+  if (this.isModified("password")) {        //check if current instance of password is modified 
+    this.password = bcrypt.hashSync(this.password, 12); //hash the current instance of password using bcrypt
+  }
+  next();
 });
 
 const User_data = mongoose.model("USER_DATA", userSchema); //export schema to mongoDB
--- server/router/auth.js
+++ server/router/auth.js
@@ -10,7 +10,8 @@ router.get("/", (req, res) => {
   res.send("Hello from the server router");
 }); //shown if homepage is visited
 
-//start of post method to send data to database
+//********************start of post method to signup*********************//
+
 router.post("/signup", async (req, res) => {
   const { fName, lName, email, password } = req.body; //object destructuring, Eg.: req.body.name = name
 
@@ -31,10 +32,35 @@ router.post("/signup", async (req, res) => {
     const user = new userSchema({ fName, lName, email, password }); //create new object of type userSchema
 
     const userCreate = await user.save(); //send new user's data to database
+
     if (userCreate) {
       res.status(201).json({ message: "User registered succesfully" });
     } else {
       res.status(500).json({ error: "Failed to register user" });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//********************start of post method to login*********************/
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body; //object destructuring
+
+    if (!email || !password) {
+      //if email or password field is empty
+      return res
+        .status(400)
+        .json({ error: "Please fill both email and password." });
+    }
+
+    const userExists = await userSchema.findOne({ email: email }); //find if user exists
+    if (!userExists) {
+      res.status(400).json({ error: "User does not exist" });
+    } else {
+      res.status(200).json({ message: "User login successful" });
     }
   } catch (err) {
     console.log(err);
