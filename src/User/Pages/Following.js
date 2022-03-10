ec13272e16071689dd180a7f6f62b98264dc1283
Shreyans Joshi <80451337+joshishreyans@users.noreply.github.com>
2022-03-10 23:09:01 +0530

    Added Material UI

--- src/User/Pages/Following.js
+++ src/User/Pages/Following.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import React from "react";
+import Avatar from "F:/GitHub/College_quora/node_modules/@mui/material/Avatar";
 
 function Following() {
-  return <div>Hi this is following</div>;
+  return (
+    <div>
+      <div class="flex items-center">
+        <Avatar
+          class="mr-10"
+          alt="Bhavya"
+          src="/static/images/avatar/1.jpg"
+        ></Avatar>
+      </div>
+      <h3>Hii this is following</h3>
+      {/* header/}
+
+    <img class="w-full object-contain" src="https://images.unsplash.com/photo-1453728013993-6d66e9c9123a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8dmlld3xlbnwwfHwwfHw%3D&w=1000&q=80%22%3E</img>
+    {/ image */}
+
+      <h4>caption</h4>
+    </div>
+  );
 }
 
 export default Following;
