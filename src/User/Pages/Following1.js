ca57177b80be8cddf99060aeb9d4836de7ebfc28
Bhavya Parekh <bhavyavilinparekh@gmail.com>
2022-03-15 23:38:44 +0530

    increased max width of post

--- src/User/Pages/Following1.js
+++ src/User/Pages/Following1.js
@@ -2,7 +2,8 @@ import React from 'react';
 import Avatar from 'D:/Studies/Github/Pandora/node_modules/@mui/material/Avatar';
 
 function Following1({ username, caption, imageUrl}) {
-  return <div class="max-w-lg bg-white border border-gray-200 mr-5 mb-11">
+  return <div className="post">
+  <div class="max-w-xl bg-white border border-gray-200 mr-5 mb-11 shadow-md">
     <div class="flex items-center px-5">
       <Avatar 
         class="px-2"
@@ -17,6 +18,7 @@ function Following1({ username, caption, imageUrl}) {
     <img class="w-full object-contain border-t-gray-700 border-b-gray-700" src= { imageUrl } alt=""></img>
     {/* image */}
       <h4 class="font-normal p-5"><strong> {username} </strong> {caption} </h4>
+  </div>
   </div>;
 }
 
